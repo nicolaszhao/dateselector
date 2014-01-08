59108cc8f8b52f959e3db6868cbc254f2e9336ca
Nicolas Zhao <nicolas-zhao@hotmail.com>
2014-01-08 13:45:28 +0800

    add unit test

--- test/dateselector-test.js
+++ test/dateselector-test.js
@@ -1,62 +1,65 @@
-(function($) {
-  /*
-    ======== A Handy Little QUnit Reference ========
-    http://api.qunitjs.com/
-
-    Test methods:
-      module(name, {[setup][ ,teardown]})
-      test(name, callback)
-      expect(numberOfAssertions)
-      stop(increment)
-      start(decrement)
-    Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      throws(block, [expected], [message])
-  */
-
-  module('jQuery#dateselector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is chainable', function() {
-    expect(1);
-    // Not a bad test to run on collection methods.
-    strictEqual(this.elems.dateselector(), this.elems, 'should be chainable');
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    strictEqual(this.elems.dateselector().text(), 'awesome0awesome1awesome2', 'should be awesome');
-  });
-
-  module('jQuery.dateselector');
-
-  test('is awesome', function() {
-    expect(2);
-    strictEqual($.dateselector(), 'awesome.', 'should be awesome');
-    strictEqual($.dateselector({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
-  });
-
-  module(':dateselector selector', {
-    // This will run before each test in this module.
-    setup: function() {
-      this.elems = $('#qunit-fixture').children();
-    }
-  });
-
-  test('is awesome', function() {
-    expect(1);
-    // Use deepEqual & .get() when comparing jQuery objects.
-    deepEqual(this.elems.filter(':dateselector').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  });
-
+( function($) {
+	
+	module('dateselector: options');
+	
+	test('dateFormat', function() {
+		expect(3);
+		
+		var $date = $('#dateselector').dateselector({
+			dateFormat: 'yy-mm-dd'
+		});
+		
+		equal($date.val(), '1949-01-01', 'yy-mm-dd: ' + $date.val());
+		
+		$date.dateselector({
+			dateFormat: 'm/d/y'
+		});
+		equal($date.val(), '1/1/49', 'm/d/y: ' + $date.val());
+		
+		$date.dateselector({
+			dateFormat: 'mm, yy'
+		});
+		equal($date.val(), '01, 1949', 'mm, yy: ' + $date.val());
+	});
+	
+	test('yearRange', function() {
+		expect(3);
+		
+		var $date = $('#dateselector').dateselector(),
+			thisYear = new Date().getFullYear(),
+			
+			yearRange = function(format) {
+				var $year,
+					ret;
+				
+				$date.dateselector({
+					yearRange: format
+				});
+				
+				$year = $date.prev('.dateselector').find('.dateselector-year');
+				
+				ret = $year.val();
+				$year.find('option:last').prop('selected', true);
+				ret += ':' + $year.val();
+				return ret;
+			};
+		
+		equal(yearRange('-10:+10'), (thisYear - 10) + ':' + (thisYear + 10), '-10:+10 -> ' + (thisYear - 10) + ':' + (thisYear + 10));
+		equal(yearRange('1982:2009'), '1982:2009', 'nnnn:nnnn -> 1982:2009');
+		equal(yearRange('1982:-10'), '1982:' + (thisYear - 10), 'nnnn:-10 -> 1982:' + (thisYear - 10));		
+	});
+	
+	module('dateselector: methods');
+	
+	test('option', function() {
+		var $date = $('#dateselector').dateselector(),
+			thisYear = new Date().getFullYear();
+		
+		$date.dateselector('option', {
+			dateFormat: 'yy-mm-dd',
+			yearRange: '-10:+10'
+		});
+		equal($date.val(), (thisYear - 10) + '-01-01', $date.val());
+	});
+	
 }(jQuery));
